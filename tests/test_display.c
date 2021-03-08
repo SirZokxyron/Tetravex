54451f43b28ce47844c19d659837aa8c1596f8ee
SirZokxyron <sir.zokxyron@gmail.com>
2021-03-08 22:32:14 +0100

    modified test accordingly to recent changes in display func

--- tests/test_display.c
+++ tests/test_display.c
@@ -2,14 +2,21 @@
 
 int main(int argc, char * argv[]) {
     //> Check number of argument
-    if (argc != 2) FATAL_ERROR("Test Main - A .tetra file was expected as argument.", 2);
+    if (argc != 3) FATAL_ERROR("Test Main - A .tetra file and a debug mode (0-1) were expected as arguments.", 2);
 
     //> Read the grid from the given file
     grid tetravex = read_grid(argv[1]);
 
-    //> Debug the grid and square content
-    for(int square_i = 0; square_i < (tetravex.size * tetravex.size); square_i++) {
-        debug_square(tetravex, tetravex.tab[square_i], square_i);        
+    switch (atoi(argv[2])) {
+        case 1:
+            for(int square_i = 0; square_i < (tetravex.size * tetravex.size); square_i++) {
+                debug_square(tetravex, tetravex.tab[square_i], square_i); 
+            }
+            break;
+        case 0:
+        default:
+            print_grid(tetravex);
+            break;
     }
 
     return 0;
